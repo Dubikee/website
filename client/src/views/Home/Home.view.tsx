42b8be8380c2c0e24a0232d9ab08fe13616c2aec
luokun <485386599@qq.com>
2018-08-02 11:38:09 +0800

    .

--- client/src/views/Home/Home.view.tsx
+++ client/src/views/Home/Home.view.tsx
@@ -9,28 +9,26 @@ class HomeView extends PComponent {
         inputLeft: 0
     };
     render() {
-        return (
-            <div className="home">
-                <Row className="form-row" type="flex" justify="center">
-                    <Col xs={18} sm={12} md={10} lg={8} xl={6} xxl={4} className='form-col'>
-                        <h1>LOGIN</h1>
-                        <Form className="login-form">
-                            <Form.Item>
-                                <Input prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />} placeholder="Account" />
-                            </Form.Item>
-                            <Form.Item>
-                                <Input prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />} type="password" placeholder="Password" />
-                            </Form.Item>
-                            <Form.Item className="form-item-last">
-                                <Checkbox>Remember me</Checkbox>
-                                <br />
-                                <Button type="primary" htmlType="submit" className="login-form-button" > Log in </Button>
-                            </Form.Item>
-                        </Form>
-                    </Col>
-                </Row>
-            </div>
-        );
+        return <div className="home">
+            <Row className="form-row" type="flex" justify="center">
+                <Col xs={18} sm={12} md={10} lg={8} xl={6} xxl={4} className='form-col'>
+                    <h1>LOGIN</h1>
+                    <Form className="login-form">
+                        <Form.Item>
+                            <Input prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />} placeholder="Account" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />} type="password" placeholder="Password" />
+                        </Form.Item>
+                        <Form.Item className="form-item-last">
+                            <Checkbox>Remember me</Checkbox>
+                            <br />
+                            <Button type="primary" htmlType="submit" className="login-form-button" > Log in </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </div>
     }
 }
 
