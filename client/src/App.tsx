60d0fc0e49f493e494f9a6a9b31ea3d7fcdc8740
luokun <485386599@qq.com>
2018-08-14 21:15:03 +0800

    删除无用的文件

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import { Provider } from "mobx-react";
 import { ServicesStore } from "./services";
-import { PComponent, renderRouter,React } from "./utils/core";
+import { PComponent, renderRouter, React } from "./utils/core";
 import 'antd/dist/antd.css';
-import './assets/iconfont/iconfont.css'
 import { routes, redirect } from "./routes";
 export default class App extends PComponent {
-    render() {
-        return <Provider {...ServicesStore}>
-            {renderRouter(routes, redirect)}
-        </Provider>
-    }
+	render() {
+		return <Provider {...ServicesStore}>
+			{renderRouter(routes, redirect)}
+		</Provider>
+	}
 }
